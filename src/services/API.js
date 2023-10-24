e611fe67fe19d55404aba9d70cba696edfcb08d1
lanping <904315628@qq.com>
2023-10-24 19:02:00 +0800

    https added

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export default(url='http://aa1f1d3f1df22476cbf3887045f4b00a-595466472.ap-southeast-1.elb.amazonaws.com') =>{
+export default(url='https://aa1f1d3f1df22476cbf3887045f4b00a-595466472.ap-southeast-1.elb.amazonaws.com') =>{
     return axios.create({
         baseURL:url,
     })
